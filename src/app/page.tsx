1ecdf0e8dee87e3d9aec9b441532dc0618a129f4
Maksym <maksymhuczynski@gmail.com>
2025-02-13 22:52:04 +0100

    scroll10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div></div>
       <Hero />
       <Matrix />
-      {/* <Features /> */}
+      <Features />
       {/* <Testimonials /> */}
       {/* <Testimonials2 /> */}
       {/* <CTA /> */}
